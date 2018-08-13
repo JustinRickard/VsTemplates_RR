2621c3f2f74b42050e5e759565e215fddeab5b74
Justin Rickard <justin@river.red>
2018-08-13 18:00:40 +0100

    Adding command and query templates

---
+++ commands/CmdCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RedRiver.Saffron.AspNetCore;
+using RedRiver.Saffron.Cqrs;
+
+namespace CruiseLine.Shared.Templates
+{
+    public class CreateENTITYCommand : CommandBase
+    {
+    }
+
+    public class CreateENTITYCommandHandler : CommandHandlerBase<CreateENTITYCommand>
+    {
+        private readonly CoreContext _context;
+
+        public CreateENTITYCommandHandler(CoreContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task ExecuteAsync(CreateENTITYCommand command)
+        {
+
+
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
